5913543f1d9dcb4e9da0b16f5fd24bd282564596
Dimo Petrov <dimo.petrov@tick42.com>
2023-03-12 15:02:27 +0200

    do the intro, if-else, functions, vecs, variables, and primiteve types exirsices

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,14 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count: i32) -> i32 {
+    return
+        if apple_count <= 40 {
+            apple_count * 2
+        } else {
+            apple_count
+        };
+}
 
 // Don't modify this function!
 #[test]
